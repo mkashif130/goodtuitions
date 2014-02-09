afe8491fac01f3b0c4ca5178b56c87e6a64354c0
unknown <clicktoarslan390@gmail.com>
2014-02-10 01:41:59 +0500

    commit on feb 10

---
+++ GoogdTuitions/DataAccessLayer/User/ChangePasswordEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace DataAccessLayer.User
+{
+    public class ChangePasswordEntity
+    {
+        public string EmailAdderss { get; set; }
+        public string CurrentPassword { get; set; }
+        public string NewPassword { get; set; }
+    }
+}
