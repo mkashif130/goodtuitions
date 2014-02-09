c2522e5d85f82041fc0b1a923d081e6d7982628d
unknown <clicktoarslan390@gmail.com>
2014-02-10 01:43:26 +0500

    commit on feb 10

---
+++ GoogdTuitions/GoodTuitionsWebsite/Scripts/master.js
@@ -0,0 +1,74 @@
+﻿$(document).ready(function () {
+});
+
+function ChangePassword() {
+    var currentPassword = $('#InputCurrentPassword').val();
+    var newPassword = $('#InputNewPassword').val();
+    var confirmNewPassword = $('#InputConfirmNewPassword').val();
+
+    if (currentPassword == '') {
+        AlertInfo('Please enter your current password.');
+        $('#InputCurrentPassword').focus();
+        return;
+    }
+    if (currentPassword.length < 6) {
+        AlertInfo('Password must be greater then or equal to six digits.');
+        $('#InputCurrentPassword').focus();
+        return;
+    }
+    if (newPassword == '') {
+        AlertInfo('Please enter your current password.');
+        $('#InputNewPassword').focus();
+        return;
+    }
+    if (confirmNewPassword == '') {
+        AlertInfo('Please confirm your current password.');
+        $('#InputConfirmNewPassword').focus();
+        return;
+    }
+    if (newPassword.length < 6) {
+        AlertInfo('Password must be greater then or equal to six digits.');
+        $('#InputNewPassword').focus();
+        return;
+    }
+
+    if (newPassword != confirmNewPassword) {
+        AlertInfo('Password did not match.');
+        $('#InputNewPassword').focus();
+        return;
+    }
+
+    var changePassword = {
+        CurrentPassword: currentPassword,
+        NewPassword: newPassword
+    };
+
+    $('#hLoading').removeClass("hide");
+
+    $.ajax({    // ajax start
+        type: "POST",
+        url: GetAPIUrl('ChangePassword'),
+        contentType: 'application/json; charset=utf-8',
+        data: JSON.stringify(changePassword),
+        success: function (data, status, xhr) {
+            $('#hLoading').addClass("hide");
+            var response = xhr.statusText;
+            if (response != null) {
+                if (response == 'OK') { // 200 code for successfully creation
+                    // redirect to success page
+                    AlertSuccess("Password changed successfully!");
+                }
+                else {
+                    //redirect to error page
+                    AlertError(response);
+                }
+            }
+            else {
+                AlertError('Uknown Error');
+            }
+        },
+        error: function () {
+            $('#hLoading').addClass("hide");
+        }
+    }); // ajax end
+}
---
+++ GoogdTuitions/GoodTuitionsWebsite/Theme/custom-js/get-selected-theme-color.js
@@ -0,0 +1,49 @@
+﻿function GetColor(css) {
+    var def = '#00708c';
+    if (css == 'color-default.css') {
+        return "#00708c";
+    }
+    if (css == 'color-niceblue.css') {
+        return '#008cba';
+    }
+    if (css == 'color-intenseblue.css') {
+        return "#0093e8";
+    }
+    if (css == 'color-otherblue.css') {
+        return '#429ab4';
+    }
+    if (css == 'color-blue.css') {
+        return '#00439e';
+    }
+    if (css == 'color-puregreen.css') {
+        return '#068100';
+    }
+    if (css == 'color-grassgreen.css') {
+        return '#5a9900';
+    }
+    if (css == 'color-green.css') {
+        return '#008c73';
+    }
+    if (css == 'color-olive.css') {
+        return '#88a026';
+    }
+    if (css == 'color-gold.css') {
+        return '#a79c42';
+    }
+    if (css == 'color-orange.css') {
+        return '#d77400';
+    }
+    if (css == 'color-pink.css') {
+        return '#db46e4';
+    }
+    if (css == 'color-fuchsia.css') {
+        return "#cc0261";
+    }
+    if (css == 'color-violet.css') {
+        return '#5400ab';
+    }
+    if (css == 'color-red.css') {
+        return '#a00000';
+    }
+    return def;
+}
